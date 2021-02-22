12c412dadf37362472378cacb8f66680fa84c0a7
PatrickBoynton <jpboynton3@gmail.com>
2021-02-22 14:39:04 -0500

    Added localhost to the urls.

--- conf/settings.py
+++ conf/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "django-restaurant-app-jpb3.herokuapp.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "django-restaurant-app-jpb3.herokuapp.com"]
 
 
 # Application definition
