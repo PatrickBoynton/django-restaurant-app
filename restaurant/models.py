afe9cd730dcdbfeee0d4cf501f908b2c4d1f457f
PatrickBoynton <jpboynton3@gmail.com>
2021-02-22 16:23:04 -0500

    Added an item model.

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class MenuItem(models.Model):
+    item = models.CharField(max_length=255)
+    quantity = models.IntegerField()
+    price = models.FloatField()
+
+    def __str__(self):
+        return self.item
