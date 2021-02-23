aa6497301cfcaee08d0306dec2493cd155cd99bb
PatrickBoynton <jpboynton3@gmail.com>
2021-02-23 13:25:41 -0500

    Added serializers for the order and menu items.

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from rest_framework import generics
+from .models import MenuItem, Order
+from .serializers import MenuItemSerializer, OrderSerializer
+
 
 # Create your views here.
+
+class OrderListAPIView(generics.ListAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+
+class OrderDetailAPIView(generics.RetrieveAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+
+class MenuItemListAPIView(generics.ListAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+
+
+class MenuItemDetailAPIView(generics.RetrieveAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
