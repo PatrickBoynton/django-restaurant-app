3c6c4051646fc9e54eb33119b2413022b94b5dca
PatrickBoynton <jpboynton3@gmail.com>
2021-02-23 12:28:54 -0500

    Changed the menu item, added an order model.

--- restaurant/models.py
+++ restaurant/models.py
@@ -4,8 +4,13 @@ from django.db import models
 # Create your models here.
 class MenuItem(models.Model):
     item = models.CharField(max_length=255)
-    quantity = models.IntegerField()
     price = models.FloatField()
 
     def __str__(self):
         return self.item
+
+
+class Order(models.Model):
+    item = models.CharField(max_length=255)
+    quantity = models.IntegerField()
+    price = models.FloatField()
