a1605b687b32d83a63ce61ac7c3b4f9b8b5d3ab0
PatrickBoynton <jpboynton3@gmail.com>
2021-02-23 20:40:45 -0500

    Added a post request.

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
 from .models import MenuItem, Order
 from .serializers import MenuItemSerializer, OrderSerializer
 
@@ -11,10 +12,18 @@ class OrderListAPIView(generics.ListAPIView):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
 
+    def post(self, request, format=None):
+        serializer = OrderSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+
 
 class OrderDetailAPIView(generics.RetrieveAPIView):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
+
 
 
 class MenuItemListAPIView(generics.ListAPIView):
