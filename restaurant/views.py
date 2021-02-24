0a5137ab531b51b91a49cbe2251ff9cd61e74d10
PatrickBoynton <jpboynton3@gmail.com>
2021-02-24 11:41:01 -0500

    Removed the post method, and changed the inheritance.

--- restaurant/views.py
+++ restaurant/views.py
@@ -8,22 +8,14 @@ from .serializers import MenuItemSerializer, OrderSerializer
 
 # Create your views here.
 
-class OrderListAPIView(generics.ListAPIView):
+class OrderListAPIView(generics.ListCreateAPIView):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
-
-    def post(self, request, format=None):
-        serializer = OrderSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
 
 
 class OrderDetailAPIView(generics.RetrieveAPIView):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
-
 
 
 class MenuItemListAPIView(generics.ListAPIView):
